6aa66333f541280b3dc279b2b7ad488c571a57ba
HasnaAkter <hasnaakter.cse@gmail.com>
2024-02-04 10:18:51 +0600

    server

--- public/server.js
+++ public/server.js
@@ -1,21 +1,18 @@
-//backend/server.js
+
 let express = require('express');
 let mongoose = require('mongoose');
 let cors = require('cors');
 let bodyParser = require('body-parser');
-let dbConfig = require('./database/db');
-
-// Express Route
+let dbConfig = require('./database/db')
 const studentRoute =
 	require('../backend/routes/student.route')
 
-// Configure mongoDB Database
+
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-// Connecting MongoDB Database
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.db)
 	.then(() => {
